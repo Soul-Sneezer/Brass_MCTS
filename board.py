e0105b93567afbde62d635d524536caf1a6bb6d2
Soul Sneezer <petrerobertcristian@gmail.com>
2024-11-17 20:05:55 +0200

    Added cities

--- board.py
+++ board.py
@@ -34,6 +34,11 @@ class IndustryType(Enum):
     COTTONMILL = 4
     POTTERY = 5
 
+class LinkType(Enum):
+    BOTH = 0
+    RAIL = 1
+    CANAL = 2
+
 class TradingHub:
     def __init__(self, name, bonus, buildings):
         self.name = name
@@ -54,49 +59,50 @@ class BuildingSlot:
         self.player_id = player_id
 
 class Square:
-    def __init__(self):
-        self.building_slots = []
+    def __init__(self, buildings):
+        self.building_types = buildings
     
-    def add_building_slot(self, building_slot):
-        self.building_slots.append(building_slot)
-
 class City:
     def __init__(self, name):
         self.name = name
         self.adjacent = {}
+        self.squares = []
+    
+    def add_square(self, square):
+        self.squares.append(square)
 
     def add_neighbor(self, neighbor, connection_type):
         self.adjacent[neighbor] = connection_type
 
-buildings = [[[IndustryType.IRONWORKS,( 3, 3, 1), (5, 1, 0), 0, 4],
-              [IndustryType.IRONWORKS,( 5, 3, 1), (7, 1, 0), 0, 4],
-              [IndustryType.IRONWORKS,( 7, 2, 1), (9, 1, 0), 0, 5],
-              [IndustryType.IRONWORKS,( 9, 1, 1),(12, 1, 0), 0, 6]],
-             [[IndustryType.COALMINE,( 1, 4, 2), (5, 0, 0), 0, 2],
-              [IndustryType.COALMINE,( 2, 7, 1), (7, 0, 0), 0, 3],
-              [IndustryType.COALMINE,( 3, 6, 1), (8, 0, 1), 0, 4],
-              [IndustryType.COALMINE,( 4, 5, 1),(10, 0, 1), 0, 5]],
-             [[IndustryType.BREWERY,( 4, 4, 2), (5, 0, 1), 0, 1],
-              [IndustryType.BREWERY,( 5, 5, 2), (7, 0, 1), 0, 1],
-              [IndustryType.BREWERY,( 7, 5, 2), (9, 0, 1), 0, 1],
-              [IndustryType.BREWERY,(10,5, 2), (9, 0, 1), 0, 1]], # will have the modify the number of resources produced to 2 in second phase
-             [[IndustryType.MANUFACTORY,( 5, 5, 1), (12, 0, 0), 1, 0],
-              [IndustryType.MANUFACTORY,( 5, 4, 2), (14, 1, 0), 1, 0],
-              [IndustryType.MANUFACTORY,( 9, 3, 1), (16, 1, 1), 1, 0],
-              [IndustryType.MANUFACTORY,(12, 2, 1), (18, 1, 1), 1, 0]],
-             [[IndustryType.POTTERY,(10, 5, 1), (17, 0, 1), 1, 0],
-              [IndustryType.POTTERY,( 1, 1, 1), ( 0, 1, 0), 1, 0],
-              [IndustryType.POTTERY,(11, 5, 1), (22, 2, 0), 2, 0],
-              [IndustryType.POTTERY,( 1, 1, 1), ( 0, 1, 0), 1, 0],
-              [IndustryType.POTTERY,(20, 5, 1), (24, 2, 0), 2, 0]],
-             [[IndustryType.COTTONMILL,( 3, 5, 2), ( 8, 1, 0), 1, 0],
-              [IndustryType.COTTONMILL,( 5, 1, 1), (10, 0, 1), 1, 0],
-              [IndustryType.COTTONMILL,( 4, 4, 0), (12, 2, 0), 0, 0],
-              [IndustryType.COTTONMILL,( 3, 6, 1), ( 8, 0, 1), 1, 0],
-              [IndustryType.COTTONMILL,( 8, 2, 2), (16, 1, 0), 2, 0],
-              [IndustryType.COTTONMILL,( 7, 6, 1), (20, 0, 0), 1, 0],
-              [IndustryType.COTTONMILL,( 9, 4, 0), (16, 1, 1), 0, 0],
-              [IndustryType.COTTONMILL,(11, 1, 0), (20, 0, 2), 1, 0]]]
+buildings = [[[IndustryType.IRONWORKS,( 3, 3, 1), (5, 1, 0), 0, 4, 1],
+              [IndustryType.IRONWORKS,( 5, 3, 1), (7, 1, 0), 0, 4, 1],
+              [IndustryType.IRONWORKS,( 7, 2, 1), (9, 1, 0), 0, 5, 1],
+              [IndustryType.IRONWORKS,( 9, 1, 1),(12, 1, 0), 0, 6, 1]],
+             [[IndustryType.COALMINE,( 1, 4, 2), (5, 0, 0), 0, 2, 1],
+              [IndustryType.COALMINE,( 2, 7, 1), (7, 0, 0), 0, 3, 2],
+              [IndustryType.COALMINE,( 3, 6, 1), (8, 0, 1), 0, 4, 2],
+              [IndustryType.COALMINE,( 4, 5, 1),(10, 0, 1), 0, 5, 2]],
+             [[IndustryType.BREWERY,( 4, 4, 2), (5, 0, 1), 0, 1, 2],
+              [IndustryType.BREWERY,( 5, 5, 2), (7, 0, 1), 0, 1, 2],
+              [IndustryType.BREWERY,( 7, 5, 2), (9, 0, 1), 0, 1, 2],
+              [IndustryType.BREWERY,(10, 5, 2), (9, 0, 1), 0, 1, 1]], # will have the modify the number of resources produced to 2 in second phase
+             [[IndustryType.MANUFACTORY,( 5, 5, 1), (12, 0, 0), 1, 0, 3],
+              [IndustryType.MANUFACTORY,( 5, 4, 2), (14, 1, 0), 1, 0, 2],
+              [IndustryType.MANUFACTORY,( 9, 3, 1), (16, 1, 1), 1, 0, 3],
+              [IndustryType.MANUFACTORY,(12, 2, 1), (18, 1, 1), 1, 0, 3]],
+             [[IndustryType.POTTERY,(10, 5, 1), (17, 0, 1), 1, 0, 1],
+              [IndustryType.POTTERY,( 1, 1, 1), ( 0, 1, 0), 1, 0, 1],
+              [IndustryType.POTTERY,(11, 5, 1), (22, 2, 0), 2, 0, 1],
+              [IndustryType.POTTERY,( 1, 1, 1), ( 0, 1, 0), 1, 0, 1],
+              [IndustryType.POTTERY,(20, 5, 1), (24, 2, 0), 2, 0, 1]],
+             [[IndustryType.COTTONMILL,( 3, 5, 2), ( 8, 1, 0), 1, 0, 1],
+              [IndustryType.COTTONMILL,( 5, 1, 1), (10, 0, 1), 1, 0, 2],
+              [IndustryType.COTTONMILL,( 4, 4, 0), (12, 2, 0), 0, 0, 1],
+              [IndustryType.COTTONMILL,( 3, 6, 1), ( 8, 0, 1), 1, 0, 1],
+              [IndustryType.COTTONMILL,( 8, 2, 2), (16, 1, 0), 2, 0, 2],
+              [IndustryType.COTTONMILL,( 7, 6, 1), (20, 0, 0), 1, 0, 1],
+              [IndustryType.COTTONMILL,( 9, 4, 0), (16, 1, 1), 0, 0, 1],
+              [IndustryType.COTTONMILL,(11, 1, 0), (20, 0, 2), 1, 0, 2]]]
 
 class Board:
     iron_works = []
@@ -106,30 +112,218 @@ class Board:
     potteries = []
     cotton_mills = []
 
+    cities = []
+
     def __init__(self):
         pass
     
+    def createBuildingType(self, i):
+        for building in buildings[i]:
+            for i in range(building[5]):
+                industry_type = building[0]
+                stats = building[1]
+                price = building[2]
+                beers = building[3]
+                resources = building[4]
+                new_ironwork = Building(industry_type, stats, price, beers, resources)
+                self.iron_works.append(new_ironwork)
+    
     def createIronWorks(self):
-        building = Building(IndustryType.IRONWORKS, 4, 3, 1, (7, 1, 0) , 1, 0, 4)
-        self.iron_works.append(building)
-        building = Building(IndustryType.IRONWORKS, )
+        self.createBuildingType(0)
     def createCoalMines(self):
-        pass 
+        self.createBuildingType(1) 
     def createBreweries(self):
-        pass
+        self.createBuildingType(2)
     def createManufactories(self):
-        pass 
+        self.createBuildingType(3)
     def createPotteries(self):
-        pass 
+        self.createBuildingType(4)
     def createCottonMills(self):
-        pass
-    
+        self.createBuildingType(5)
+   
     def createBuildings(self):
-        createIronWorks(self)
-        createCoalMines(self)
-        createBreweries(self)
-        createManufactories(self)
-        createPotteries(self)
-        createCottonMills(self)
+        self.createIronWorks()
+        self.createCoalMines()
+        self.createBreweries()
+        self.createManufactories()
+        self.createPotteries()
+        self.createCottonMills()
 
-    
+    def createCities(self):
+        stoke_on_trent = City("Stoke-On-Trent")
+        leek = City("Leek")
+        belper = City("Belper")
+        stone = City("Stone")
+        uttoxeter = City("Uttoxeter")
+        derby = City("Derby")
+        stafford = City("Stafford")
+        burton_on_trent = City("Burton-On-Trent")
+        cannock = City("Cannock")
+        tamworth = City("Tamworth")
+        coalbrookdale = City("Coalbrookdale")
+        wolverhampton = City("Wolverhampton")
+        walsall = City("Walsall")
+        nuneaton = City("Nuneaton")
+        dudley = City("Dudley")
+        birmingham = City("Birmingham")
+        coventry = City("Coventry")
+        kidderminster = City("Kidderminster")
+        worcester = City("Worcester")
+        redditch = City("Redditch")
+        unnamed_city0 = City("City0")
+        unnamed_city1 = City("City1")
+
+        stoke_on_trent.add_square(Square((IndustryType.MANUFACTORY, IndustryType.COTTONMILL)))
+        stoke_on_trent.add_square(Square((IndustryType.POTTERY, IndustryType.IRONWORKS)))
+        stoke_on_trent.add_square(Square((IndustryType.COTTONMILL)))
+
+        leek.add_square(Square((IndustryType.MANUFACTORY, IndustryType.COTTONMILL)))
+        leek.add_square(Square((IndustryType.MANUFACTORY, IndustryType.COALMINE)))
+
+        belper.add_square(Square((IndustryType.MANUFACTORY, IndustryType.COTTONMILL)))
+        belper.add_square(Square((IndustryType.COALMINE)))
+        belper.add_square(Square((IndustryType.POTTERY)))
+
+        stone.add_square(Square((IndustryType.MANUFACTORY, IndustryType.BREWERY)))
+        stone.add_square(Square((IndustryType.COTTONMILL, IndustryType.COALMINE)))
+
+        uttoxeter.add_square(Square((IndustryType.COTTONMILL, IndustryType.BREWERY)))
+        uttoxeter.add_square(Square((IndustryType.MANUFACTORY, IndustryType.BREWERY)))
+
+        derby.add_square(Square((IndustryType.MANUFACTORY, IndustryType.BREWERY)))
+        derby.add_square(Square((IndustryType.MANUFACTORY, IndustryType.COTTONMILL)))
+        derby.add_square(Square((IndustryType.IRONWORKS)))
+
+        burton_on_trent.add_square(Square((IndustryType.COTTONMILL, IndustryType.COALMINE)))
+        burton_on_trent.add_square(Square((IndustryType.BREWERY)))
+
+        tamworth.add_square(Square((IndustryType.MANUFACTORY, IndustryType.COALMINE)))
+        tamworth.add_square(Square((IndustryType.MANUFACTORY, IndustryType.COALMINE)))
+
+        walsall.add_square(Square((IndustryType.IRONWORKS, IndustryType.COTTONMILL)))
+        walsall.add_square(Square((IndustryType.COTTONMILL, IndustryType.BREWERY)))
+
+        birmingham.add_square(Square((IndustryType.MANUFACTORY, IndustryType.COTTONMILL)))
+        birmingham.add_square(Square((IndustryType.COTTONMILL)))
+        birmingham.add_square(Square((IndustryType.IRONWORKS)))
+        birmingham.add_square(Square((IndustryType.COTTONMILL)))
+
+        nuneaton.add_square(Square((IndustryType.COTTONMILL, IndustryType.BREWERY)))
+        nuneaton.add_square(Square((IndustryType.MANUFACTORY, IndustryType.COALMINE)))
+
+        coventry.add_square(Square((IndustryType.POTTERY)))
+        coventry.add_square(Square((IndustryType.COTTONMILL, IndustryType.COALMINE)))
+        coventry.add_square(Square((IndustryType.IRONWORKS, IndustryType.COTTONMILL)))
+
+        redditch.add_square(Square((IndustryType.COTTONMILL, IndustryType.COALMINE)))
+        redditch.add_square(Square((IndustryType.IRONWORKS)))
+
+        stafford.add_square(Square((IndustryType.COTTONMILL, IndustryType.BREWERY)))
+        stafford.add_square(Square((IndustryType.POTTERY)))
+
+        cannock.add_square(Square((IndustryType.COTTONMILL, IndustryType.COALMINE)))
+        cannock.add_square(Square((IndustryType.COALMINE)))
+
+        unnamed_city0.add_square(Square((IndustryType.BREWERY)))
+
+        wolverhampton.add_square(Square((IndustryType.COTTONMILL)))
+        wolverhampton.add_square(Square((IndustryType.COTTONMILL, IndustryType.COALMINE)))
+
+        coalbrookdale.add_square(Square((IndustryType.IRONWORKS, IndustryType.BREWERY)))
+        coalbrookdale.add_square(Square((IndustryType.IRONWORKS))) 
+        coalbrookdale.add_square(Square((IndustryType.COALMINE)))
+
+        dudley.add_square(Square((IndustryType.COALMINE)))
+        dudley.add_square(Square((IndustryType.IRONWORKS)))
+
+        kidderminster.add_square(Square((IndustryType.MANUFACTORY, IndustryType.COALMINE)))
+        kidderminster.add_square(Square((IndustryType.MANUFACTORY)))
+
+        worcester.add_square(Square((IndustryType.MANUFACTORY)))
+        worcester.add_square(Square((IndustryType.MANUFACTORY)))
+
+        unnamed_city1.add_square(Square((IndustryType.BREWERY)))
+
+        stoke_on_trent.add_neighbor(stone, LinkType.BOTH)
+        stoke_on_trent.add_neighbor(leek, LinkType.BOTH)
+        
+        leek.add_neighbor(belper, LinkType.RAIL)
+        leek.add_neighbor(stoke_on_trent, LinkType.BOTH)
+
+        belper.add_neighbor(leek, LinkType.RAIL)
+        belper.add_neighbor(derby, LinkType.BOTH)
+
+        derby.add_neighbor(belper, LinkType.BOTH)
+        derby.add_neighbor(burton_on_trent, LinkType.BOTH)
+        derby.add_neighbor(uttoxeter, LinkType.RAIL)
+
+        uttoxeter.add_neighbor(derby, LinkType.RAIL)
+        uttoxeter.add_neighbor(stone, LinkType.RAIL)
+
+        burton_on_trent.add_neighbor(derby, LinkType.BOTH)
+        burton_on_trent.add_neighbor(stone, LinkType.BOTH)
+        burton_on_trent.add_neighbor(tamworth, LinkType.BOTH)
+        burton_on_trent.add_neighbor(cannock, LinkType.RAIL)
+        burton_on_trent.add_neighbor(walsall, LinkType.CANAL)
+
+        stone.add_neighbor(burton_on_trent, LinkType.BOTH)
+        stone.add_neighbor(stafford, LinkType.BOTH)
+        stone.add_neighbor(uttoxeter, LinkType.RAIL)
+        stone.add_neighbor(stoke_on_trent, LinkType.BOTH)
+
+        stafford.add_neighbor(stone, LinkType.BOTH)
+        stafford.add_neighbor(cannock, LinkType.BOTH)
+
+        cannock.add_neighbor(stafford, LinkType.BOTH)
+        cannock.add_neighbor(wolverhampton, LinkType.BOTH)
+        cannock.add_neighbor(unnamed_city0, LinkType.BOTH)
+        cannock.add_neighbor(burton_on_trent, LinkType.RAIL)
+
+        unnamed_city0.add_neighbor(cannock, LinkType.BOTH)
+
+        tamworth.add_neighbor(burton_on_trent, LinkType.BOTH)
+        tamworth.add_neighbor(nuneaton, LinkType.BOTH)
+        tamworth.add_neighbor(birmingham, LinkType.BOTH)
+        tamworth.add_neighbor(walsall, LinkType.RAIL)
+
+        nuneaton.add_neighbor(tamworth, LinkType.BOTH)
+        nuneaton.add_neighbor(birmingham, LinkType.RAIL)
+        nuneaton.add_neighbor(coventry, LinkType.RAIL)
+
+        coventry.add_neighbor(nuneaton, LinkType.RAIL)
+        coventry.add_neighbor(birmingham, LinkType.BOTH)
+
+        birmingham.add_neighbor(coventry, LinkType.BOTH)
+        birmingham.add_neighbor(nuneaton, LinkType.RAIL)
+        birmingham.add_neighbor(tamworth, LinkType.BOTH)
+        birmingham.add_neighbor(walsall, LinkType.BOTH)
+        birmingham.add_neighbor(redditch, LinkType.RAIL)
+        birmingham.add_neighbor(dudley, LinkType.BOTH)
+        birmingham.add_neighbor(worcester, LinkType.BOTH)
+
+        walsall.add_neighbor(birmingham, LinkType.BOTH)
+        walsall.add_neighbor(tamworth, LinkType.RAIL)
+        walsall.add_neighbor(burton_on_trent, LinkType.CANAL)
+        walsall.add_neighbor(cannock, LinkType.BOTH)
+        walsall.add_neighbor(wolverhampton, LinkType.BOTH)
+
+        dudley.add_neighbor(birmingham, LinkType.BOTH)
+        dudley.add_neighbor(wolverhampton, LinkType.BOTH)
+        dudley.add_neighbor(kidderminster, LinkType.BOTH)
+
+        redditch.add_neighbor(birmingham, LinkType.RAIL)
+
+        worcester.add_neighbor(birmingham, LinkType.BOTH)
+        worcester.add_neighbor(kidderminster, LinkType.BOTH)
+        worcester.add_neighbor(unnamed_city1, LinkType.BOTH)
+
+        unnamed_city1.add_neighbor(worcester, LinkType.BOTH)
+        unnamed_city1.add_neighbor(kidderminster, LinkType.BOTH)
+
+        wolverhampton.add_neighbor(walsall, LinkType.BOTH)
+        wolverhampton.add_neighbor(dudley, LinkType.BOTH)
+        wolverhampton.add_neighbor(coalbrookdale, LinkType.BOTH)
+        wolverhampton.add_neighbor(cannock, LinkType.BOTH)
+
+        coalbrookdale.add_neighbor(wolverhampton, LinkType.BOTH)
+        coalbrookdale.add_neighbor(kidderminster, LinkType.BOTH)
